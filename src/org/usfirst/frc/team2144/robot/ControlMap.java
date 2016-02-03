c620f1cb891cbfd846640e3c9a764fb1fee0b4ee
mommothazaz123 <mommothazaz123@gmail.com>
2016-02-03 10:18:49 -0800

    comments

--- src/org/usfirst/frc/team2144/robot/ControlMap.java
+++ src/org/usfirst/frc/team2144/robot/ControlMap.java
@@ -3,11 +3,11 @@ package org.usfirst.frc.team2144.robot;
 public class ControlMap {
 
 	// left stick
-	public static final int spoolButton = 1;
-	public static final int intakeButton = 3;
+	public static final int spoolButton = 1; // hold to spool intake for fire
+	public static final int intakeButton = 3; // hold to spool intake for intaking
 
 	// right stick
-	public static final int fireButton = 1;
-	public static final int intakePitchButton = 2;
+	public static final int fireButton = 1; // press to move servo to fire pos
+	public static final int intakePitchButton = 2; // hold to move intake to intake position
 
 }
