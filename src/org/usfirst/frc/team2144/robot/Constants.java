60b66667638f8e6b564613eec250b181c1c9aa8a
mommothazaz123 <mommothazaz123@gmail.com>
2016-02-07 11:33:38 -0800

    added Constants; test 1 code

---
+++ src/org/usfirst/frc/team2144/robot/Constants.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team2144.robot;
+
+public class Constants {
+	
+	// linear actuator up/down
+	public static final double actUp = 0.06;
+	public static final double actDown = 0.6;
+	
+}
