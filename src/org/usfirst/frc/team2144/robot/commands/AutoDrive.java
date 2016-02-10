cde906274c36831d92dafbd250d5427b090e99f2
Andrew Zhu <mommothazaz123@gmail.com>
2016-02-09 17:34:36 -0800

    began auto

---
+++ src/org/usfirst/frc/team2144/robot/commands/AutoDrive.java
@@ -0,0 +1,58 @@
+package org.usfirst.frc.team2144.robot.commands;
+
+/**
+ *
+ */
+public class AutoDrive extends CommandBase {
+
+	double leftSpeed, rightSpeed;
+	int leftDistance = 0, rightDistance = 0;
+
+	public AutoDrive(double leftSpd, double rightSpd) {
+		// Use requires() here to declare subsystem dependencies
+		requires(drivetrain);
+		leftSpeed = leftSpd;
+		rightSpeed = rightSpd;
+	}
+
+	public AutoDrive(double leftSpd, double rightSpd, int leftDist, int rightDist) {
+		requires(drivetrain);
+		leftSpeed = leftSpd;
+		rightSpeed = rightSpd;
+		leftDistance = leftDist;
+		rightDistance = rightDist;
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		drivetrain.tankDrive(false, leftSpeed, rightSpeed);
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		if (drivetrain.have_encoders_reached_pos(leftDistance, rightDistance)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		if (leftDistance == 0 && rightDistance == 0) {
+			
+		} else {
+			drivetrain.tankDrive(false, 0, 0);
+			drivetrain.reset_encoders();
+		}
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
