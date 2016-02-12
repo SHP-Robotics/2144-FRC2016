913579348b4d3a720775940787c94fd936508120
Andrew Zhu <mommothazaz123@gmail.com>
2016-02-12 14:39:26 -0800

    updated constants

--- src/org/usfirst/frc/team2144/robot/Constants.java
+++ src/org/usfirst/frc/team2144/robot/Constants.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team2144.robot;
 public class Constants {
 
 	// linear actuator up/down
-	public static final double actUp = 0.04;
+	public static final double actUp = 0.065;
 	public static final double actDown = 0.62;
 	public static final double actHalfDown = 0.40;
 
